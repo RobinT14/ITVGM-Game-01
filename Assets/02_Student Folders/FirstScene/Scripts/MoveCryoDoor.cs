68b93307f6522ac2546cf268f6f47ca8dd849aa8
amarisesilie <91152459+amarisesilie@users.noreply.github.com>
2021-12-30 18:11:31 +0100

    fixed door issue
    door wouldn't open lol oop

--- Assets/02_Student Folders/FirstScene/Scripts/MoveCryoDoor.cs
+++ Assets/02_Student Folders/FirstScene/Scripts/MoveCryoDoor.cs
@@ -7,8 +7,8 @@ public class MoveCryoDoor : MonoBehaviour
     public Transform Door;
     public float speed = 2f;
     Vector3 newPosition = new Vector3(-2, -1.896437f, -1);
-    public Animation anim;
-    public string animationName;
+    //public Animation anim;
+    //public string animationName;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +19,10 @@ public class MoveCryoDoor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!anim.IsPlaying(animationName))
-        {
+        //if (!anim.IsPlaying(animationName))
+        //{
             transform.localPosition = Vector3.MoveTowards(transform.localPosition, newPosition, Time.deltaTime * speed);
-        }
+        //}
         // Door.Translate(Vector3.forward * Time.deltaTime * speed);
 
     }
