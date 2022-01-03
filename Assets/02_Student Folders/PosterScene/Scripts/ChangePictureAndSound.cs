fa16b2651f8aeda4e15195f6065ef3bb25c0a346
RT <robin.the@xs4all.nl>
2022-01-03 03:15:52 +0100

    Statische beelden icm voice over

--- Assets/02_Student Folders/PosterScene/Scripts/ChangePictureAndSound.cs
+++ Assets/02_Student Folders/PosterScene/Scripts/ChangePictureAndSound.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class ChangePictureAndSound : MonoBehaviour
 {
+	public int Canvas01, Canvas02;
 	void Start()
 	{
 		//Start the coroutine we define below named ExampleCoroutine.
@@ -14,10 +15,10 @@ public class ChangePictureAndSound : MonoBehaviour
 
 	IEnumerator ExampleCoroutine()
 	{
-		yield return new WaitForSeconds(2);
+		yield return new WaitForSeconds(Canvas01);
 		GameObject.Find("Canvas02").SetActive(true);
 		GameObject.Find("Canvas01").SetActive(false);
-		yield return new WaitForSeconds(14);
+		yield return new WaitForSeconds(Canvas02);
 		SceneManager.LoadScene("Scene1");
 		// GameObject.Find("Canvas02").SetActive(false);
 	}
